99f91f4f6bbae56d87249bb9b3f2ab313f0265b0
mehrnoor <saheledarya140001@gmail.com>
2026-02-05 19:38:15 +0330

    8

--- issue1.py
+++ issue1.py
@@ -1,3 +1,5 @@
-import pandas as pd
-import requests
-hello
+gh issue list -R $repo --state closed --limit 1000 |
+ForEach-Object {
+  $num = ($_ -split "`t")[0]
+    gh issue delete $num -R $repo --yes
+ }
